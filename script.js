1455a67b33ab4ec8a3716fd9fd7d7ce6e8ad7209
Simone Arcari <simonearcari@outlook.it>
2023-05-26 21:57:28 +0200

    ripetizioni

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ var linkedinSideBarEl = document.getElementById('linkedinSideBar');
 var githubSideBarEl = document.getElementById('githubSideBar');
 var youtubeSideBarEl = document.getElementById('youtubeSideBar');
 var instagramSideBarEl = document.getElementById('instagramSideBar');
+var ripetizioniSideBarEl = document.getElementById('ripetizioniSideBar');
 
 var textHeaderEl = document.getElementById('header-text');
 
@@ -21,11 +22,14 @@ var longHeaderText = 'Simone Arcari - Computer Science/Automation Control Engine
 var middleHeaderText = 'Simone Arcari | Automation Control Engineer';
 var shortHeaderText = 'Simone Arcari';
 var idx = 1;
-var speed = 300 / 6;
-var minWidth_longText = 1400;
-var minWidth_middleText = 1000;
-var minWidth_shortText = 350;
+var speed = 20;
+var minWidth_longText = 1150;
+var minWidth_middleText = 800;
+var minWidth_shortText = 300;
+var criticalWidth = 890;    // ricorda che i paramentri di .container sono {width: 700px, max-width: 800px}
 
+writeHeaderText();
+changeContainerWidth()
 
 /* HANDLER */
 
@@ -49,8 +53,12 @@ window.addEventListener('resize', function () {
     changeHeaderText();
 });
 
+// Esegui il codice JavaScript quando la finestra del browser viene ridimensionata
+window.addEventListener('resize', function () {
+    changeContainerWidth();
+});
 
-writeHeaderText();
+
 
 // selezione dark theme
 iconTheme.onclick = function () {
@@ -62,7 +70,6 @@ iconTheme.onclick = function () {
         iconTheme.src = "img/moon.png";
     }
 }
-
 
 // Imposta il valore del margin-top dell'elemento subito sotto l'header
 function setMyElementMarginTop() {
@@ -80,7 +87,7 @@ function setMyElementMarginTop() {
     nextElement.style.marginTop = headerHeight + 'px';
 }
 
-// Scrive un po' alla volta l' header text
+// Scrive un po' alla volta l' head6er text
 function writeHeaderText() {
     let currentText = longHeaderText;
 
@@ -125,7 +132,22 @@ function changeHeaderText() {
     textHeaderEl.innerText = currentText;
 }
 
-// funzione per espandere il testo visualizzato nel riquadro Elettronica
+// cambia il valore width di .container in maniera opportuna
+function changeContainerWidth() {
+    let pageWidth = window.innerWidth;
+    
+    if (pageWidth < criticalWidth) {
+        // Seleziona tutti gli elementi con la classe desiderata
+        let elements = document.querySelectorAll('.container');
+
+        // Itera su tutti gli elementi e modifica il parametro width
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].style.width = (pageWidth-80) + 'px';
+        }
+    }
+}
+
+// funzione per espandere i div .container
 function moreText(buttonId) {
 
     switch (buttonId) {
@@ -146,10 +168,8 @@ function moreText(buttonId) {
             break;
     }
 
-
     let moreTextEl = document.getElementById(moreTextId);
     let moreTextButtonEl = document.getElementById(buttonId);
-
 
     if (moreTextEl.style.display == "none") {
         moreTextEl.style.display = "inline";
@@ -160,6 +180,30 @@ function moreText(buttonId) {
         moreTextButtonEl.innerHTML = "Read more";
     }
 }
+
+
+
+// Link cliccabili (tasti menu bar)
+linkedinSideBarEl.addEventListener('click', function () {
+    window.open("https://www.linkedin.com/in/simone-arcari-14319b25b/", '_blank');
+})
+
+githubSideBarEl.addEventListener('click', function () {
+    window.open("https://github.com/simone-arcari", '_blank');
+})
+youtubeSideBarEl.addEventListener('click', function () {
+    window.open("https://www.youtube.com/channel/UCRFpI_W660ps-DDMBMKz83A", '_blank');
+})
+
+instagramSideBarEl.addEventListener('click', function () {
+    window.open("https://www.instagram.com/simone_arcari/", '_blank');
+})
+
+ripetizioniSideBarEl.addEventListener('click', function () {
+    window.open("ripetizioni.html", '_blank');
+})
+
+
 
 // Link cliccabili (tasti tondi)
 linkedinEl.addEventListener('click', function () {
@@ -180,18 +224,3 @@ instagramEl.addEventListener('click', function () {
 
 
 
-// Link cliccabili (tasti menu bar)
-linkedinSideBarEl.addEventListener('click', function () {
-    window.open("https://www.linkedin.com/in/simone-arcari-14319b25b/", '_blank');
-})
-
-githubSideBarEl.addEventListener('click', function () {
-    window.open("https://github.com/simone-arcari", '_blank');
-})
-youtubeSideBarEl.addEventListener('click', function () {
-    window.open("https://www.youtube.com/channel/UCRFpI_W660ps-DDMBMKz83A", '_blank');
-})
-
-instagramSideBarEl.addEventListener('click', function () {
-    window.open("https://www.instagram.com/simone_arcari/", '_blank');
-})
