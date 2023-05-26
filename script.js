dfc8715cdc3436f778a2966caa5d9ed0022b9a8b
Simone Arcari <simonearcari@outlook.it>
2023-05-26 20:26:35 +0200

    read more

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ var minWidth_middleText = 1000;
 var minWidth_shortText = 350;
 
 
-                        /* HANDLER */
+/* HANDLER */
 
 // Esegui il codice JavaScript quando la funzione writeText() termina il suo compito
 document.addEventListener('writeText', function () {
@@ -53,12 +53,12 @@ window.addEventListener('resize', function () {
 writeHeaderText();
 
 // selezione dark theme
-iconTheme.onclick = function() {
+iconTheme.onclick = function () {
     document.body.classList.toggle("dark-theme");
 
-    if(document.body.classList.contains("dark-theme")) {
+    if (document.body.classList.contains("dark-theme")) {
         iconTheme.src = "img/sun.png";
-    }else {
+    } else {
         iconTheme.src = "img/moon.png";
     }
 }
@@ -84,15 +84,15 @@ function setMyElementMarginTop() {
 function writeHeaderText() {
     let currentText = longHeaderText;
 
-    if(textHeaderEl.offsetWidth <= minWidth_longText) {   // se la largezza (che dipende dal dispositivo) è piccola -> testo medio
+    if (textHeaderEl.offsetWidth <= minWidth_longText) {   // se la largezza (che dipende dal dispositivo) è piccola -> testo medio
         currentText = middleHeaderText;
     }
 
-    if(textHeaderEl.offsetWidth <= minWidth_middleText) {   // se la largezza (che dipende dal dispositivo) è troppo piccola -> testo corto
+    if (textHeaderEl.offsetWidth <= minWidth_middleText) {   // se la largezza (che dipende dal dispositivo) è troppo piccola -> testo corto
         currentText = shortHeaderText;
     }
 
-    if(textHeaderEl.offsetWidth <= minWidth_shortText) {   // se la largezza (che dipende dal dispositivo) è estremamente piccola -> testo nullo
+    if (textHeaderEl.offsetWidth <= minWidth_shortText) {   // se la largezza (che dipende dal dispositivo) è estremamente piccola -> testo nullo
         currentText = '';
     }
 
@@ -101,7 +101,7 @@ function writeHeaderText() {
     if (idx <= currentText.length) {
         setTimeout(writeHeaderText, speed);
         document.dispatchEvent(writeTextEvent);
-    }else {
+    } else {
         idx = 1;
     }
 }
@@ -110,52 +110,88 @@ function writeHeaderText() {
 function changeHeaderText() {
     let currentText = longHeaderText;
 
-    if(textHeaderEl.offsetWidth <= minWidth_longText ) {   // se la largezza (che dipende dal dispositivo) è piccola -> testo medio
+    if (textHeaderEl.offsetWidth <= minWidth_longText) {   // se la largezza (che dipende dal dispositivo) è piccola -> testo medio
         currentText = middleHeaderText;
     }
 
-    if(textHeaderEl.offsetWidth <= minWidth_middleText) {   // se la largezza (che dipende dal dispositivo) è troppo piccola -> testo corto
+    if (textHeaderEl.offsetWidth <= minWidth_middleText) {   // se la largezza (che dipende dal dispositivo) è troppo piccola -> testo corto
         currentText = shortHeaderText;
     }
 
-    if(textHeaderEl.offsetWidth <= minWidth_shortText) {   // se la largezza (che dipende dal dispositivo) è estremamente piccola -> testo nullo
+    if (textHeaderEl.offsetWidth <= minWidth_shortText) {   // se la largezza (che dipende dal dispositivo) è estremamente piccola -> testo nullo
         currentText = '';
     }
 
     textHeaderEl.innerText = currentText;
 }
 
+// funzione per espandere il testo visualizzato nel riquadro Elettronica
+function moreText(buttonId) {
+
+    switch (buttonId) {
+        case "moreTextButtonElettronica":
+            moreTextId = "moreTextElettronica";
+            break;
+
+        case "moreTextButtonAboutMe":
+            moreTextId = "moreTextAboutMe";
+            break;
+
+        case "orange":
+            console.log("Selected fruit is an orange.");
+            break;
+
+        default:
+            console.log("Selected fruit is unknown.");
+            break;
+    }
+
+
+    let moreTextEl = document.getElementById(moreTextId);
+    let moreTextButtonEl = document.getElementById(buttonId);
+
+
+    if (moreTextEl.style.display == "none") {
+        moreTextEl.style.display = "inline";
+        moreTextButtonEl.innerHTML = "Read less";
+    }
+    else {
+        moreTextEl.style.display = "none";
+        moreTextButtonEl.innerHTML = "Read more";
+    }
+}
+
 // Link cliccabili (tasti tondi)
-linkedinEl.addEventListener('click', function() {
+linkedinEl.addEventListener('click', function () {
     window.open("https://www.linkedin.com/in/simone-arcari-14319b25b/", '_blank');
 })
 
-githubEl.addEventListener('click', function() {
+githubEl.addEventListener('click', function () {
     window.open("https://github.com/simone-arcari", '_blank');
 })
 
-youtubeEl.addEventListener('click', function() {
+youtubeEl.addEventListener('click', function () {
     window.open("https://www.youtube.com/channel/UCRFpI_W660ps-DDMBMKz83A", '_blank');
 })
 
-instagramEl.addEventListener('click', function() {
+instagramEl.addEventListener('click', function () {
     window.open("https://www.instagram.com/simone_arcari/", '_blank');
 })
 
 
 
 // Link cliccabili (tasti menu bar)
-linkedinSideBarEl.addEventListener('click', function() {
+linkedinSideBarEl.addEventListener('click', function () {
     window.open("https://www.linkedin.com/in/simone-arcari-14319b25b/", '_blank');
 })
 
-githubSideBarEl.addEventListener('click', function() {
+githubSideBarEl.addEventListener('click', function () {
     window.open("https://github.com/simone-arcari", '_blank');
 })
-youtubeSideBarEl.addEventListener('click', function() {
+youtubeSideBarEl.addEventListener('click', function () {
     window.open("https://www.youtube.com/channel/UCRFpI_W660ps-DDMBMKz83A", '_blank');
 })
 
-instagramSideBarEl.addEventListener('click', function() {
+instagramSideBarEl.addEventListener('click', function () {
     window.open("https://www.instagram.com/simone_arcari/", '_blank');
 })
